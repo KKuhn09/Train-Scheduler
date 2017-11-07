f737f2b836b559daf98e23d8764ee61c5cf13a55
Kieran Kuhn <kuhn.kieran@gmail.com>
2017-11-07 15:28:11 -0700

    Added Comments

--- app.js
+++ app.js
@@ -7,61 +7,59 @@ var config = {
     storageBucket: "train-scheduler-9a8de.appspot.com",
     messagingSenderId: "17599582323"
   };
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
+//Store firebase in convenient variable
+var database = firebase.database();
 
-  var database = firebase.database();
-
-  var name = "";
-  var destination = "";
-  var firstTrain = "";
-  var frequency = "";
+var name = "";
+var destination = "";
+var firstTrain = "";
+var frequency = "";
 
 $(document).ready(function(){
 
-	//Whenever the page loads or the values in the database change
-	// the following function will run
+	//Read data from database
 	database.ref().on("value", function(snapshot){
-
 		$("#train").html(snapshot.val().name);
 		$("#destination").html(snapshot.val().destination);
 		$("#time").html(snapshot.val().time);
-		//$("#frequency").html(snapshot.val().frequency);
+		$("#frequency").html(snapshot.val().frequency);
 
 	}, function(errorObject){
 		console.log("The READ failed: " + errorObject.code);
 	});
 
-	//Whenever the #add-button button is clicked, the function will run
+	//Whenever #add-button button is clicked
 	$("#add-button").on("click", function(event){
 
 		event.preventDefault();
-
-		//Grabs the values from the users input and stores them into variables
+		//Store values from user input
 		var aName = $("#train").val().trim();
 		var aDestination = $("#destination").val().trim();
 		var aFirstTrain = moment($("#time").val(), "HH:mm").subtract(10, "years").format("X");
+		console.log(aFirstTrain);
 		var aFrequency = $("#frequency").val().trim();
-
+		//Add those values to the database
 		database.ref().push({
 			name: aName,
 			destination: aDestination,
 			firstTrain: aFirstTrain,
 			frequency: aFrequency,
 		});
-
+		//Clear user input area
 		$("#train").val("");
 		$("#destination").val("");
 		$("#time").val("");
 		$("#frequency").val("");
 	});
-
+	//For every post on the database, and every time a new child is added
 	database.ref().on("child_added", function(childSnapshot, prevChildKey) {
-
+		//Store values from database
 		var bName = childSnapshot.val().name;
 		var bDestination = childSnapshot.val().destination;
 		var bFirstTrain = childSnapshot.val().firstTrain;
 		var bFrequency = childSnapshot.val().frequency;
-
+		//Calculate train times
 		var differenceTimes = moment().diff(moment.unix(bFirstTrain), "minutes");
 		var bRemainder = moment().diff(moment.unix(bFirstTrain), "minutes") % bFrequency ;
 		var bMinAway = bFrequency - bRemainder;
@@ -70,7 +68,7 @@ $(document).ready(function(){
 		console.log(differenceTimes);
 		console.log(bNextArrival);
 		console.log(bMinAway);
-
+		//Display the trains info
 		$("#train-info").append(
 			"<tr><td>" + bName + "</td><td>" + bDestination + "</td><td>" + bFrequency + "</td><td>" + 
 			bNextArrival + "</td><td>" + bMinAway + "</td></tr>");
